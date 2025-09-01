aea2da01cddddd1a46442c0688b5a923445a25f6
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2025-09-01 23:49:17 +0900

    fix: パッケージ化する際に定数をインライン化
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- packages/amplify-auth-hooks/rolldown.config.ts
+++ packages/amplify-auth-hooks/rolldown.config.ts
@@ -5,6 +5,9 @@ export default defineConfig([
   {
     external: [/^node:/, /^@aws-amplify\//, /^@types\//, /^@xstate\//, /^react\//, 'react', 'react-dom', 'xstate'],
     treeshake: true,
+    optimization: {
+      inlineConst: true,
+    },
     input: { index: 'src/index.ts', machines: 'src/machines/index.ts', authenticator: 'src/authenticator/index.ts' },
     output: [{ format: 'esm', entryFileNames: '[name].mjs', sourcemap: true }],
     plugins: [
