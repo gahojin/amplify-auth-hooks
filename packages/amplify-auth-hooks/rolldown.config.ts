4b4a0e4149016dad8ef9c8177c26028c41ee8a6d
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2025-11-16 11:45:31 +0900

    build: 出力先の削除をrolldownの機能で行う
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- packages/amplify-auth-hooks/rolldown.config.ts
+++ packages/amplify-auth-hooks/rolldown.config.ts
@@ -9,7 +9,7 @@ export default defineConfig([
       inlineConst: true,
     },
     input: { index: 'src/index.ts', machines: 'src/machines/index.ts', authenticator: 'src/authenticator/index.ts' },
-    output: [{ format: 'esm', entryFileNames: '[name].mjs', sourcemap: true }],
+    output: [{ dir: 'dist', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true, cleanDir: true }],
     plugins: [
       IsolatedDecl({
         transformOptions: {
