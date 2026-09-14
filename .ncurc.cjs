eb98bad7efc34243aecb002f1c64cc30e93c0bf9
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-09-14 10:37:59 +0900

    build: publish出来ないため、pnpm v12をv11にロールバック
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('npm-check-updates').RcOptions } */
 module.exports = {
   target: (name) => {
-    if (name === '@types/node') {
+    if (name === 'pnpm' || name === '@types/node') {
       return 'minor'
     }
     return 'newest'
